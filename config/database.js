81d8f7f23bfd495779ce200dee1db5f8f27bf15c
Skizziedevs <kmblak8@gmail.com>
2025-08-26 06:54:06 -0700

    first commit

--- config/database.js
+++ config/database.js
@@ -4,12 +4,10 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false } // required for Neon
 });
+
 
 // Test connection
 pool.on('connect', () => {
